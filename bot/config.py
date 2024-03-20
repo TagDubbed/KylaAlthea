5f1a4d079900fb01971a7311f2dba577428aaf40
TagDubbed <159412599+TagDubbed@users.noreply.github.com>
2024-03-21 02:20:55 +0800

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,17 +1,17 @@
 from os import environ as env
 
 class Telegram:
-    API_ID = int(env.get("TELEGRAM_API_ID", 26739167))
-    API_HASH = env.get("TELEGRAM_API_HASH", "9bcf49d320560e6623dc4764a666d8e0")
-    OWNER_ID = int(env.get("OWNER_ID", 1019675595))
+    API_ID = int(env.get("TELEGRAM_API_ID", 21317013))
+    API_HASH = env.get("TELEGRAM_API_HASH", "c80881d5f279958d9e4112520df1c880")
+    OWNER_ID = int(env.get("OWNER_ID", 6803967314))
     ALLOWED_USER_IDS = env.get("ALLOWED_USER_IDS", "1019675595").split()
-    BOT_USERNAME = env.get("TELEGRAM_BOT_USERNAME", "lucy_jewel_bot")
-    BOT_TOKEN = env.get("TELEGRAM_BOT_TOKEN", "6854815463:AAGSoZr-WwOoSriINbHxA1GGpDjesYXgb2I")
-    CHANNEL_ID = int(env.get("TELEGRAM_CHANNEL_ID", -1002014702047))
+    BOT_USERNAME = env.get("TELEGRAM_BOT_USERNAME", "allecolbot")
+    BOT_TOKEN = env.get("TELEGRAM_BOT_TOKEN", "6680952567:AAG1SzfJ2CEJGlmMtP6FToLfXEImZ-OYPzU")
+    CHANNEL_ID = int(env.get("TELEGRAM_CHANNEL_ID", -1002012472003))
     SECRET_CODE_LENGTH = int(env.get("SECRET_CODE_LENGTH", 12))
 
 class Server:
-    BASE_URL = env.get("BASE_URL", "https://tagkdrama03-de9f31c7041a.herokuapp.com")
+    BASE_URL = env.get("BASE_URL", "https://tagkdrama01-aff5b23afa36.herokuapp.com")
     BIND_ADDRESS = env.get("BIND_ADDRESS", "0.0.0.0")
     PORT = int(env.get("PORT", 8080))
 
