915dacaa4b7e612f7ffada4f4783ccf013d4db43
TagDubbed <159412599+TagDubbed@users.noreply.github.com>
2024-03-21 06:06:34 +0800

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -11,7 +11,7 @@ class Telegram:
     SECRET_CODE_LENGTH = int(env.get("SECRET_CODE_LENGTH", 12))
 
 class Server:
-    BASE_URL = env.get("BASE_URL", "https://tagkdrama01-aff5b23afa36.herokuapp.com")
+    BASE_URL = env.get("BASE_URL", "https://tagdubbed-8a6deedf3105.herokuapp.com")
     BIND_ADDRESS = env.get("BIND_ADDRESS", "0.0.0.0")
     PORT = int(env.get("PORT", 8080))
 
